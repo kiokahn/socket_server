13ce32fcfe2bf60b3af7c90f134396dd4ec9c9fd
kiokahn <kiokahn@gazzi.ai>
2024-06-13 14:39:12 +0900

    fixed data transfer issues

--- socket_server.py
+++ socket_server.py
@@ -7,21 +7,20 @@
 #
 
 import os
-import time
 import socket
 from datetime import datetime
 
 class SocketServer:
     def __init__(self):
-        self.bufsize = 1024
-        file = open('./response.bin', 'rb')
-        self.RESPONSE = file.read()
-        file.close()
+        self.bufsize = 1024  # 버퍼 크기 설정
+        with open('./response.bin', 'rb') as file:
+            self.RESPONSE = file.read()  # 응답 파일 읽기
 
         self.DIR_PATH = './request'
         self.createDir(self.DIR_PATH)
-        
+
     def createDir(self, path):
+        """디렉토리 생성"""
         try:
             if not os.path.exists(path):
                 os.makedirs(path)
@@ -29,47 +28,49 @@ class SocketServer:
             print("Error: Failed to create the directory.")
 
     def run(self, ip, port):
-        # create socket
+        """서버 실행"""
+        # 소켓 생성
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((ip, port))
+        self.sock.listen(10)
         print("Start the socket server...")
         print("\"Ctrl+C\" for stopping the server!\r\n")
 
-        self.sock.bind((ip, port))
-        self.sock.listen(10)
-
         try:
             while True:
-                # wait request
+                # 클라이언트의 요청 대기
                 clnt_sock, req_addr = self.sock.accept()
-
+                clnt_sock.settimeout(5.0)  # 타임아웃 설정 (5초)
                 print("Request message...\r\n")
-                # read and print request
+
                 response = b""
-                while True:
-                    data = clnt_sock.recv(self.bufsize)
-                    if len(data)<self.bufsize :
+                try:
+                    while True:
+                        data = clnt_sock.recv(self.bufsize)
+                        if not data:
+                            break
                         response += data
-                        break
-                    else:
-                        response += data
-                        time.sleep(0.01)
+                except socket.timeout:
+                    print("Connection timed out")
 
                 now = datetime.now()
                 filename = self.DIR_PATH + now.strftime('/%Y-%m-%d-%H-%M-%S.bin')
-                file = open(filename, 'wb')
-                file.write(response)
-                file.close()
-                print(response)
+                with open(filename, 'wb') as file:
+                    file.write(response)
 
-                # send response
+                print("Received data:")
+                print(response.decode('utf-8', errors='replace'))
+
+                # 응답 전송
                 clnt_sock.sendall(self.RESPONSE)
 
-                # close clnt_sock
+                # 클라이언트 소켓 닫기
                 clnt_sock.close()
         except KeyboardInterrupt:
             print("\r\nStop the server...")
 
-        # close socket
+        # 서버 소켓 닫기
         self.sock.close()
 
 if __name__ == "__main__":
